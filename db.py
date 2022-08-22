d88860e536d4ebca94086cdcad650744cca7184e
clinton letsoela <clintonletsoela724c@gmail.com>
2022-08-22 13:29:07 +0200

    modifications

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 import pymssql
 
-conn = pymssql.connect(server='designdb.database.windows.net', 
-                user='designdb-admin@designdb', 
-                password='Design2022', 
-                database='RawData')
+def connectionPool ():
+            conn = pymssql.connect(server='designdb.database.windows.net', 
+                            user='designdb-admin@designdb', 
+                            password='Design2022', 
+                            database='RawData')
+            return conn,conn.cursor()
 
-cursor = conn.cursor() 
-cursor.execute("INSERT INTO RawData (ServerTimestamp, PositionDirection, PositionLatitude,PositionLongitude,PositionSpeed) VALUES ('12334', '23', '50', '45', '23');")
+connection, cursor = connectionPool()  
 
-conn.commit()
-print(conn)
+            
